7142f60ad96e04bd6924ff9b25784a7ec2fa13d7
kecramer <kcramer@gmail.com>
2018-06-04 12:24:38 -0700

    Use mailchimp for email

--- controller/reminders.js
+++ controller/reminders.js
@@ -1,22 +1,78 @@
-const db = require('../model');
+const crypto = require('crypto');
+const Mailchimp = require('mailchimp-api-v3')
+const mailchimp = new Mailchimp(process.env.MAILCHIMP_TOKEN);
+const mailchimp_lists = {
+   register: '0628fad118',
+   mail: 'b302ecacfe',
+   vote: '1d2c782785',
+}
 
 const create = (req, res) => {
-   console.log('Running create function');
-	db.Email.find({email: req.query.email}, (err, email) => {
-      if(err) {console.log(err); res.sendStatus(500);}
+   let success = false;
 
-      console.log(`Found ${JSON.stringify(email)}`);
+   if(!req.query.email) {
+      res.sendStatus(400);
+   }
+   let email = req.query.email;
 
-      if(email.length === 0) {
-         db.Email.create({email: req.query.email, reminder_1day: true, reminder_7day: true, reminder_30day: true}, (err, email) => {
-            if(err) {console.log(err); res.sendStatus(500);}
+   if(req.query.registration == 'true') {
+      console.log('Adding user to registration reminders');
+      mailchimp.put({
+         path: `/lists/${mailchimp_lists.register}/members/${crypto.createHash('md5').update(email).digest("hex")}`,
+         body: {
+            email_address: email,
+            status_if_new: 'subscribed',
+            fname: 'John',
+            lname: 'Doe'
+         }
+      }, (err, result) => {
+         if(err) {console.log(err);}
+         success = true;
+         console.log(JSON.stringify(result));
+      });
+   }
 
-            console.log('succesufully added email: ' + email.email);
-            res.sendStatus(204);
-         })
+   if(req.query.mailvote == 'true') {
+      console.log('Adding user to mail-to-vote reminders');
+      mailchimp.put({
+         path: `/lists/${mailchimp_lists.mail}/members/${crypto.createHash('md5').update(email).digest("hex")}`,
+         body: {
+            email_address: email,
+            status_if_new: 'subscribed',
+            fname: 'John',
+            lname: 'Doe'
+         }
+      }, (err, result) => {
+         if(err) {console.log(err);}
+         success = true;
+         console.log(JSON.stringify(result));
+      });
+   }
+
+   if(req.query.vote == 'true') {
+      console.log('Adding user to vote reminders');
+      mailchimp.put({
+         path: `/lists/${mailchimp_lists.vote}/members/${crypto.createHash('md5').update(email).digest("hex")}`,
+         body: {
+            email_address: email,
+            status_if_new: 'subscribed',
+            fname: 'John',
+            lname: 'Doe'
+         }
+      }, (err, result) => {
+         if(err) {console.log(err);}
+         success = true;
+         console.log(JSON.stringify(result));
+      });
+   }
+
+   setTimeout(()=> {
+      if(success) {
+         res.sendStatus(200);
+      } else {
+         res.sendStatus(400);
       }
-      res.sendStatus(200);
-   })
+   }, 1000);
 };
 
 module.exports = {
--- model/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require('mongoose');
-const connectionString = process.env.MONGODB_URI || 'mongodb://localhost/wdi-political-hackathon';
-
-mongoose.connect(connectionString);
-
-module.exports.Email = require('./email.js');
